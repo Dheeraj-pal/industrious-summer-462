f4d9eb69beefe716def1f71890d834d646721c35
dheeraj-pal <dheeraj.pal19@gmail.com>
2025-06-09 15:22:24 +0530

    server up

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const { connection } = require("./config/db");
 const { noteRouter } = require("./routes/product.route");
 const { userRouter } = require("./routes/user.routes");
 const { authenticate } = require("./middlewares/authenticate.middleware");
+require("dotenv").config() 
 
 const app = express();
 app.use(express.json());
@@ -24,9 +25,10 @@ app.use("/users", userRouter);
 // app.use(authenticate);
 app.use("/products", noteRouter);
 
-app.listen(1999, async () => {
+app.listen(process.env.PORT, async () => {
   try {
     await connection;
+    console.log("Server running on port:", process.env.PORT)
     console.log("connected to DB");
   } catch (error) {
     console.log(error, "unable to connect to DB");
